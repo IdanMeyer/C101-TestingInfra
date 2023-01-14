f0dbf34effc13b0cf312ff2ab1f449a3188498fd
Idan Meyer <idan@Idan-Laptop.localdomain>
2023-01-14 23:40:06 +0200

    Added q4 tests

--- test_ex6.py
+++ test_ex6.py
@@ -40,6 +40,11 @@ def execute_ex6_3_test(question_number, num1, expected_output):
     output = result.decode().split("Output: ")[-1].strip()
     assert expected_output == int(output)
 
+def execute_ex6_4_test(question_number, num1):
+    input_string = f"{question_number}{os.linesep}{num1}{os.linesep}"
+    result = Infra.execute_c_bin(compiled_path, input_string)
+    return "is a palindrome" in result.decode()
+
 
 class TestQuestion1(object):
     QUESTION_NUMBER = 1
@@ -98,3 +103,23 @@ class TestQuestion3(object):
     def test_random(self, number):
         number = random.randint(0, 2**15)
         execute_ex6_3_test(self.QUESTION_NUMBER, number, self.nextPowerOf2(number))
+
+class TestQuestion4(object):
+    QUESTION_NUMBER = 4
+
+    def test_sanity(self):
+        assert True==execute_ex6_4_test(self.QUESTION_NUMBER, 9)
+        assert False==execute_ex6_4_test(self.QUESTION_NUMBER, 10)
+
+    @pytest.mark.parametrize('number',range(1, 100))
+    def test_automated(self, number):
+        bin_str = bin(number)[2:]
+        is_palindrom = (bin_str ==bin_str[::-1])
+        assert is_palindrom == execute_ex6_4_test(self.QUESTION_NUMBER, number)
+
+    @pytest.mark.parametrize('number',range(1, 100))
+    def test_random(self, number):
+        number = random.randint(0, 2**15)
+        bin_str = bin(number)[2:]
+        is_palindrom = (bin_str ==bin_str[::-1])
+        assert is_palindrom == execute_ex6_4_test(self.QUESTION_NUMBER, number)
